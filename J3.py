477156bb4fbbe65b48fdf59b74415efea4395cf0
Ryan Wu <ryanwu@rooostr.ai>
2026-02-14 18:34:43 -0500

    adjusted J3

--- J3.py
+++ J3.py
@@ -1,37 +1,50 @@
 N = int(input())
 vals = []
-for i in range(N):
+for _ in range(N):
     x = input()
     rstr = ""
     total = 0
-    l, r = 0, 0
+    # Capital loop:
+    for i in x:
+        if i.isupper():
+            rstr += i
+
+    # Total number loop
+    isneg = False
+    l = 0
+    r = 0
     while r < len(x):
+        if x[r] == "-":
+            isneg = True
         if x[l].isnumeric():
             while x[r].isnumeric() and r + 1 < len(x):
                 r += 1
-            if r - l > 0:
-                total += int(x[l:r])
+            if l - r > 0:
+                if isneg:
+                    total -= int(x[l:r])
+                    print("subtracting" + x[l:r])
+                    isneg = False
+                else:
+                    total += int(x[l:r])
+                    print("subtracting" + x[l:r])
+
+                    isneg = False
             else:
-                total += int(x[r])
+                if isneg:
+                    total -= int(x[r])
+                    print("subtracting" + x[r])
+                    isneg = False
+                else:
+                    total += int(x[r])
+                    print("subtracting" + x[r])
+
+                    isneg = False
             l = r
-        if x[l] == "-":
-            while r + 1 < len(x) and x[r+1].isnumeric() :
-                r += 1
-            if r - l > 1:
-                total -= int(x[l+1:r])
-            else:
-                total -= int(x[r])
-            l = r
-        if x[r] in "abcdefghijklmopqrstuvwxyz":
-            r += 1
-            l += 1
-            continue
-        if x[r] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-            rstr += x[r]
+        if x[r] == "-":
+            isneg = True
         r += 1
         l += 1
-    while rstr[-1].isnumeric():
-        rstr = rstr[:-1:]
+
     vals.append(rstr + str(total))
 
 for val in vals:
