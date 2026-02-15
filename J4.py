d648649ab16c1f7769fe765052c2bd908b83b39a
Ryan Wu <ryanwu@rooostr.ai>
2026-02-14 20:33:11 -0500

    caught edge case and rewrote algorithm to use number to track rainy days iinstead of boolean values

--- J4.py
+++ J4.py
@@ -1,19 +1,23 @@
+import sys
 N = int(input())
 a = []
 for i in range(N):
     a.append(input())
 
 l = 0
-rain = False
+rain = 0
 maxconcec = 0
 for r in range(len(a)):
     if a[r] == "P":
-        if rain == True:
-            while a[l] != "P":
-                l += 1
-            l += 1
-        else:
-            rain = True
+        rain += 1
+        
+    while rain > 1:
+        if a[l] == "P":
+            rain -= 1
+        l += 1
+
     maxconcec = max(maxconcec, r-l + 1)
 
+if "P" not in a:
+    maxconcec -= 1
 print(maxconcec)
