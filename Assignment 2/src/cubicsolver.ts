21d21444a0a67b22678044ba51321a1d922c2f80
LMRW5 <ryan.wu5@student.tdsb.on.ca>
2026-02-24 11:37:49 -0500

    worked on implementing front end for code

--- Assignment 2/src/cubicsolver.ts
+++ Assignment 2/src/cubicsolver.ts
@@ -10,7 +10,7 @@ export function cubicRoot(a:number,b:number,c:number,d:number) {
   }
   else if (discriminant > 0){
     // Case B (1 real root and 2 imaginary)
-    res[0] = cardanoMethod(a,b,c,d,p,q)
+    res[0] = cardanoMethod(a,b,c,d,p,q).toFixed(2)
     res[1] = (-1 + imaginary*Math.sqrt(3))/2
     res[2] = (-1 - imaginary*Math.sqrt(3))/2
     
@@ -19,27 +19,30 @@ export function cubicRoot(a:number,b:number,c:number,d:number) {
   else{
     if (p == 0 && q == 0){
       // Case C (triple root)
-      res[0] = cardanoMethod(a,b,c,d,p,q)
-      res[1] = cardanoMethod(a,b,c,d,p,q)
-      res[2] = cardanoMethod(a,b,c,d,p,q)
+      res[0] = cardanoMethod(a,b,c,d,p,q).toFixed(2)
+      res[1] = cardanoMethod(a,b,c,d,p,q).toFixed(2)
+      res[2] = cardanoMethod(a,b,c,d,p,q).toFixed(2)
 
     }
     else if (p != 0){
       // Case D (double root + single root)
-      res[0] = cardanoMethod(a,b,c,d,p,q)
-      res[1] = cardanoMethod(a,b,c,d,p,q)
-      res[2] = Math.cbrt(q/2)-(b/(3*a))
+      res[0] = cardanoMethod(a,b,c,d,p,q).toFixed(2)
+      res[1] = (Math.cbrt(q/2)-(b/(3*a))).toFixed(2)
+      res[2] = (Math.cbrt(q/2)-(b/(3*a))).toFixed(2)
     }
   }
+  res[3] = p.toFixed(2)
+  res[4] = q.toFixed(2)
+  res[5] = discriminant.toFixed(2)
   return res
 }
 
 function trigonomatricMethod(a:number,b: number,c: number,d: number,p: number,q: number){
   const rval = new Array()
   const theta = (1/3)*Math.acos(-q/(2*Math.sqrt(-(p/3)*(p/3)*(p/3))))
-  rval[0] = 2*Math.sqrt((-p)/3)*Math.cos(theta)-(b/(3*a))
-  rval[1] = 2*Math.sqrt((-p)/3)*Math.cos(theta + 2*Math.PI/3)-(b/(3*a))
-  rval[2] = 2*Math.sqrt((-p)/3)*Math.cos(theta + 4*Math.PI/3)-(b/(3*a))
+  rval[0] = (2*Math.sqrt((-p)/3)*Math.cos(theta)-(b/(3*a))).toFixed(2)
+  rval[1] = (2*Math.sqrt((-p)/3)*Math.cos(theta + 2*Math.PI/3)-(b/(3*a))).toFixed(2)
+  rval[2] = (2*Math.sqrt((-p)/3)*Math.cos(theta + 4*Math.PI/3)-(b/(3*a))).toFixed(2)
   return rval
 }
 
--- Assignment 2/src/main.ts
+++ Assignment 2/src/main.ts
@@ -1,34 +1,28 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { cubicRoot } from './cubicsolver.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-    <form>
-    <label for="a">a:</label><br>
-    <input type="number" id = "a"></input><br>
-    <label for="b">b:</label><br>
-    <input type="number" id = "b"></input><br>
-    <label for="c">c:</label><br>
-    <input type="number" id = "c"></input><br>
-    <label for="d">d:</label><br>
-    <input type="number" id = "d"></input><br>
-    <input type="submit" value="Submit">
-    </form>
-  </div>
-`
+
+const btn = document.getElementById("submittedform") as HTMLInputElement;
+
+function handleClick() {
+  const a = Number((document.getElementById("a")as HTMLInputElement).value);
+  const b = Number((document.getElementById("b")as HTMLInputElement).value);
+  const c = Number((document.getElementById("c")as HTMLInputElement).value);
+  const d = Number((document.getElementById("d")as HTMLInputElement).value);
+  console.log(a)
+  console.log(cubicRoot(a,b,c,d))
+  const roots = cubicRoot(a,b,c,d);
+  (document.getElementById("Root1") as HTMLTableCellElement).textContent = roots[0];
+  (document.getElementById("Root2")as HTMLTableCellElement).textContent = roots[1];
+  (document.getElementById("Root3")as HTMLTableCellElement).textContent = roots[2];
+  (document.getElementById("p")as HTMLTableCellElement).textContent = roots[3];
+  (document.getElementById("q")as HTMLTableCellElement).textContent = roots[4];
+  (document.getElementById("discriminant")as HTMLTableCellElement).textContent = roots[5];
+
+
+}
+
+btn.addEventListener("click", handleClick());
+
+
 
